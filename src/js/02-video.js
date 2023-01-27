0a80b67379d17c00621ecbce222227dc5a3cf2a8
ZhenianaKozhan <94985681+ZhenianaKozhan@users.noreply.github.com>
2023-01-27 19:08:55 +0200

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+const onPlay = function (data) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTimeLocalStorage = localStorage.getItem("videoplayer-current-time")
+
+player.setCurrentTime(JSON.parse(currentTimeLocalStorage).seconds).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+
 
