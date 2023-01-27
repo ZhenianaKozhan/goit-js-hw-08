26cfc310dc2bfaf03d3103ba99094296bed75237
ZhenianaKozhan <94985681+ZhenianaKozhan@users.noreply.github.com>
2023-01-27 16:35:59 +0200

    task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,37 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = creatPicCardsMarkup(galleryItems);
+
+galleryContainer.addEventListener('click', onGalleryContainer);
+
+function creatPicCardsMarkup(params) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    })
+    .join('');
+}
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryContainer.addEventListener('click', onGalleryContainer);
+
+new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250
+});
+
+function onGalleryContainer(evt) {
+    evt.preventDefault();
+    const isGalleryLinkEl = evt.target.classList.contains('gallery__image')
+    if (!isGalleryLinkEl) {
+        return;
+    }
+}
