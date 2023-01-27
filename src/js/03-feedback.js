003ad839ae22af519f37d05b1afff05485a43265
ZhenianaKozhan <94985681+ZhenianaKozhan@users.noreply.github.com>
2023-01-27 20:49:33 +0200

    task 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = "feedback-form-state";
+const formData = {};
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    inputEmail: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+populateForm();
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    console.log(formData);
+}
+
+function onFormInput(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateForm() {
+    const savedFormData = localStorage.getItem(STORAGE_KEY);
+    const savedFormDataObject = JSON.parse(savedFormData);
+    if (savedFormData) {
+        refs.inputEmail.value = savedFormDataObject.email;
+        refs.textarea.value = savedFormDataObject.message;
+    }
+}
